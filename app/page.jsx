4354aedbd5a7901f492e2719cef3e86b647708fc
unknown <randalchukzwilson@gmail.com>
2025-04-10 06:17:00 +0100

    Added OurMenu 2/3

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/AboutSection/AboutSection";
 import Header from "@/components/Header/Header";
+import OurMenu from "@/components/OurMenu/OurMenu";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Header />
       <AboutSection />
+      <OurMenu />
 
     </main>
   );
