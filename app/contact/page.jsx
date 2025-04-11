a60e61c78d7d95846b22f6e1bac23df868e1f315
unknown <randalchukzwilson@gmail.com>
2025-04-11 15:19:43 +0100

    Corected header

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React from "react";
 import PageHeader from '@/public/page-header-bg.jpg';
 import DailyOffers from '@/components/DailyOffers/DailyOffers';
 import ReserveTable from '@/components/ReserveTable/ReserveTable';
@@ -6,6 +7,8 @@ import Footers from '@/components/Footers/Footers';
 
 
 export default function page() {
+
+  
   return (
     <div>
       <header className="pb-20 relative bg-fixed bg-bottom bg-cover bg-no-repeat h-[50vh] md:h-[45vh] lg:h-[50vh] overflow-hidden"
@@ -20,6 +23,7 @@ export default function page() {
         </div>
 
       </header>
+     
       <ReserveTable />
       <Footers />
     
@@ -27,5 +31,7 @@ export default function page() {
     </div>
   )
 }
+
+
 
 
