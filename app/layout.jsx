9e549e7a03afa8900e9d4124a7726c11378402bb
unknown <randalchukzwilson@gmail.com>
2025-04-14 15:31:11 +0100

    Updatimgr

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import SessionProviderWrapper from "./SessionProviderWrapper";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,11 +21,21 @@ export const metadata = {
     "Experience the art of fine confectionery with handcrafted treats, delightful desserts, and exquisite flavors made with love and precision.",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  // Fetch the session from the server
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        cz-shortcut-listen="true"
+        suppressHydrationWarning
+      >
+        <SessionProviderWrapper session={session}>
+          {children}
+        </SessionProviderWrapper>
+        <Toaster />
       </body>
     </html>
   );
