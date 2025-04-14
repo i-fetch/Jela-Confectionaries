9531d0872b5a268f09ca871d85c7fde4686af197
unknown <randalchukzwilson@gmail.com>
2025-04-14 15:55:29 +0100

    Updatimgr Stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
 import SessionProviderWrapper from "./SessionProviderWrapper";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/models/User";
-import bcrypt from "bcryptjs";
 import { connectToDB } from "./lib/ConnectDB";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
   providers: [
