b037d424112aa519d73b1dee2c47da5f72b81ee7
unknown <randalchukzwilson@gmail.com>
2025-04-15 05:09:53 +0100

    Updatimgr Stuff for Menu Navbar

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -11,13 +11,15 @@ import Dessert from './_component/Dessert';
 export default function page() {
   return (
     <div>
+ 
+
       <header className="pb-20 relative bg-fixed bg-bottom bg-cover bg-no-repeat h-[50vh] md:h-[45vh] lg:h-[50vh] overflow-hidden"
         style={{ backgroundImage: `url(${PageHeader?.src || ''})` }} >
         {/* Overlay */}
         <div className="absolute inset-0 bg-black opacity-70 z-10" />
 
         {/* Breadcrumb  */}
-        <div className='absolute inset-0 z-50 flex flex-col items-center justify-center h-full pt-20 px-4 md:pb-20'>
+        <div className='absolute inset-0 z-20 flex flex-col items-center justify-center h-full pt-20 px-4 md:pb-20'>
           <h1 className='text-gray-300 font-bold uppercase text-2xl xl:4xl'>Our Menu</h1>
           <p className='text-gray-300 text-xs xl:text-2xl font-medium space-x-1.5'> <span>Home</span>/ <span>Menu</span></p>
         </div>
--- components/JelaCafeMap/CafeMap.jsx
+++ components/JelaCafeMap/CafeMap.jsx
@@ -8,7 +8,7 @@ const CafeMap = () => {
           <iframe
             className="contact-map-size w-full h-full"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3931.581693144684!2d6.7865615!3d6.1509749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10439301cecefdc1%3A0x7f62ee163cb6bd0a!2s18%20Oguta%20Rd%2C%20%C3%89n%C3%BA%20%E1%BB%8Cn%E1%BB%8Bcha%2C%20Onitsha%20434106%2C%20Anambra!5e0!3m2!1sen!2sng!4v1692004323707!5m2!1sen!2sng"
-            allowfullscreen
+            allowfullscreen="true"
             loading="lazy"          >
           </iframe>
           {/* Map attribution */}
