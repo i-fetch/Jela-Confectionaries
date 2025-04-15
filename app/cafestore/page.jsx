220ee3b20653e6e43ca4fd05c991a5b4f26342f9
unknown <randalchukzwilson@gmail.com>
2025-04-15 13:09:04 +0100

    Corrected path

--- app/cafestore/page.jsx
+++ app/cafestore/page.jsx
@@ -9,21 +9,21 @@ const CafeStorePage = () => {
       name: "Jela Cafe Onitsha",
       address: "123 Main Street, City Center",
       image: "/free-photo-of-friends-hugging-in-front-of-the-four-boroughs-coffee-shop.jpg", // Replace with your actual image paths
-      slug: "/cafeonitsha"
+      slug: "cafeonitsha"
     },
     {
       id: 2,
       name: "Riverside Branch",
       address: "456 River Road, Waterfront District",
       image: "/cafe-rouge.jpg", // Replace with your actual image paths
-      slug: "/cafeonitsha"
+      slug: "cafeonitsha"
     },
     {
       id: 3,
       name: "Downtown Branch",
       address: "456 River Road, Waterfront District",
       image: "/la-paris-rome.jpg", // Replace with your actual image paths
-      slug: "/cafeonitsha"
+      slug: "cafeonitsha"
     }
   ]
 
